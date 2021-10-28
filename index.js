3349aa222e55eefa342bde3a4cc34c13e8f1605f
Mohammed-Ashraful <mr.ashraful2020@gmail.com>
2021-10-28 10:20:02 +0600

    new

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { MongoClient } = require('mongodb');
 require("dotenv").config();
 const cors = require('cors');
-const port = 5000;
+const port =process.env.PORT || 5000;
 const app = express();
 
 // middleWare
